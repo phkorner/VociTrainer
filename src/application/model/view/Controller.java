e10c1c427a184683a5e9b529159ff55e78fd2400
phkorner <zh693058>
2019-11-20 11:50:14 +0100

    Controller Class functionality

--- src/application/model/view/Controller.java
+++ src/application/model/view/Controller.java
@@ -1,5 +1,31 @@
 package src.application.model.view;
 
+import javafx.scene.control.Button;
+
 public class Controller {
 
+    public Button proposal1;
+    public Button proposal2;
+    public Button proposal3;
+    public Button proposal4;
+
+    public void handleclick1(){
+        System.out.println("well done");
+        proposal1.setText("test");
+    }
+
+    public void handleclick2(){
+        System.out.println("well done");
+        proposal2.setText("test");
+    }
+
+    public void handleclick3(){
+        System.out.println("well done");
+        proposal3.setText("test");
+    }
+
+    public void handleclick4(){
+        System.out.println("well done");
+        proposal4.setText("test");
+    }
 }
