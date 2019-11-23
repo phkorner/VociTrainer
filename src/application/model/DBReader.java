18a7a22d5a22060895d3357abafffe8551b2d8ae
bjoernsson <p.hartung@gmx.ch>
2019-11-23 10:51:26 +0100

    db reader

---
+++ src/application/model/DBReader.java
@@ -0,0 +1,57 @@
+package src.application.model;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+ * required is a multimap: one key and four values.
+ * i.e. Tisch --> table, sun, moon, cloud
+ *
+ * Library from:
+ * https://www.baeldung.com/guava-multimap (suggestion?)
+ *
+ * ==> or even better: read hashmap (1-1) and randomly map other words to it!!
+ */
+public class DBReader {
+
+    public static void main(String[] args) {
+
+        File file = new File("src/com/local/DE-EN.txt");
+        HashMap<String, String> map = new HashMap<String, String>(); // key-value-couple (used for right answers)
+        ArrayList<String> words = new ArrayList<String>(); // list of all translations (used for wrong answers)
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                if (line.contains(",")) {
+                    String[] parts = line.split(",");
+                    String key = parts[0];
+                    String value = parts[1];
+                    map.put(key, value); // building hashmap for correct answers
+                    words.add(parts[1]);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("src/com/local/DE-EN.txt nicht gefunden");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //output on console to check. delete later
+        System.out.println("Alle Schlüssel-Wert-Paare lauten: ");
+        System.out.println("**********************************");
+        for (String key : map.keySet())
+        {
+            System.out.println(key + " = " + map.get(key));
+        }
+        System.out.println();
+        System.out.println("Alle Wörter für die falschen Optionen sind: ");
+        System.out.println("********************************************");
+        for (String options : words) {
+            System.out.print(options + " ");
+        }
+    }
+}
