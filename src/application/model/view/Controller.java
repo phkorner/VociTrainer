1e081b913b9be03dbefdf40ddcd18361ebd85b20
phkorner <Philipp@140183>
2019-11-24 14:48:37 +0100

    Login Button

--- src/application/model/view/Controller.java
+++ src/application/model/view/Controller.java
@@ -1,7 +1,16 @@
 package src.application.model.view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 public class Controller {
 
@@ -28,10 +37,19 @@ public class Controller {
         proposal4.setText("test");
     }
 
-    public void handleclick5() {
+    public void handleclick5()  {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Please login...");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        }catch (Exception e){
+            System.out.println("Cannot start Login Page");
+        }
 
     }
-
 }
 
 
