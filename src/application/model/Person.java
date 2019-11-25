dffe5da898779124ff4a7c519fbfdcc4b30c2315
phkorner <zh693058>
2019-11-25 17:32:21 +0100

    Create new Account

---
+++ src/application/model/Person.java
@@ -0,0 +1,101 @@
+package src.application.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Model class for a Person, to Store the User-Data.
+ * Input based on Java FX Scene Builder Tutorial
+ * @author Philipp Korner
+ */
+
+public class Person {
+
+    private final StringProperty firstName;
+    private final StringProperty lastName;
+    private final StringProperty email;
+    private final StringProperty password;
+    private final StringProperty repeatPassword;
+
+
+    /**
+     * Constructor with some initial data.
+     *
+     * @param firstName
+     * @param lastName
+     */
+    public Person(String firstName, String lastName) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+
+        // Some initial dummy data, just for convenient testing.
+        this.email = new SimpleStringProperty("email Adress");
+        this.password = new SimpleStringProperty("password");
+        this.repeatPassword = new SimpleStringProperty("Repeat password");
+
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public StringProperty emailProperty() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+
+    public StringProperty passwordProperty() {
+        return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword.get();
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword.set(repeatPassword);
+    }
+
+    public StringProperty repeatPasswordProperty() {
+        return repeatPassword;
+    }
+}
---
+++ src/application/model/view/NewAccountController.java
@@ -0,0 +1,138 @@
+package src.application.model.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import src.application.model.Person;
+
+
+public class NewAccountController {
+
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField repeatPasswordField;
+
+
+    private Person person;
+    private boolean okClicked = false;
+    private Stage dialogStage;
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    /**
+     * Returns true if the user clicked OK, false otherwise.
+     *
+     * @return
+     */
+    public boolean isOkClicked() {
+
+        return okClicked;
+    }
+
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    private void handleOk() {
+        if (isInputValid()) {
+            person.setFirstName(firstNameField.getText());
+            person.setLastName(lastNameField.getText());
+            person.setEmail(emailField.getText());
+            person.setPassword(passwordField.getText());
+            person.setRepeatPassword(repeatPasswordField.getText());
+
+            okClicked = true;
+            dialogStage.close();
+
+        }
+    }
+
+
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+
+    /**
+     * Validates the user input in the text fields.
+     *
+     * @return true if the input is valid
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+            errorMessage += "No valid first name!\n";
+        }
+
+        if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+            errorMessage += "No valid last name!\n";
+        }
+
+        if (emailField.getText() == null || emailField.getText().length() == 0) {
+            errorMessage += "No valid email-Address!\n";
+        }
+
+        if (passwordField.getText() == null || passwordField.getText().length() == 0) {
+            errorMessage += "No valid password!\n";
+        }
+
+        if (repeatPasswordField.getText() == null || repeatPasswordField.getText().length() == 0) {
+            errorMessage += "No valid password repetition!\n";
+        }
+
+        if (passwordField.getText() != repeatPasswordField.getText()){
+            errorMessage += "Your password entries are not identical\n";
+        }
+
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message.
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+
+            return false;
+        }
+    }
+
+
+}
+
+
+
+
