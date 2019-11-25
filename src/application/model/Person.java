f61049b08d47c8e72f3e835aafa2a8196604c293
phkorner <zh693058>
2019-11-25 17:35:33 +0100

    Create new Account

--- src/application/model/Person.java
+++ src/application/model/Person.java
@@ -1,8 +1,6 @@
 package src.application.model;
 
-import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
