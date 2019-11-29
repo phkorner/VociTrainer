1f90454667e6e8fb266a87e349869b0772e72ebc
phkorner <Philipp@140183>
2019-11-29 19:39:55 +0100

    Create User-Account

--- src/application/model/view/Login.java
+++ src/application/model/view/Login.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
