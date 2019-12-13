bd74e596e2050487df75622e88e686c8a23fb365
phkorner <Philipp@140183>
2019-12-13 20:18:26 +0100

    Color change

--- src/application/model/view/PrimaryController.java
+++ src/application/model/view/PrimaryController.java
@@ -1,9 +1,12 @@
 package src.application.model.view;
 
+import javafx.animation.PauseTransition;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import src.application.model.FileSave;
 import src.application.model.MainApplication;
 import src.application.model.DatabaseHandler;
@@ -60,14 +63,10 @@ public class PrimaryController {
         } else {
             highlightWrongAnswer(1);
         }
+        delay();
 
-        //load new question
-        ArrayList<String> verteiler = DatabaseHandler.getDBReader().loadNewQuestion();
-        proposal1.setText(verteiler.get(0));
-        proposal2.setText(verteiler.get(1));
-        proposal3.setText(verteiler.get(2));
-        proposal4.setText(verteiler.get(3));
-        word.setText(verteiler.get(4));
+        loadNewQuestion();
+
     }
 
     public void handleclick2() {
@@ -79,14 +78,8 @@ public class PrimaryController {
         } else {
             highlightWrongAnswer(2);
         }
-
-        //load new question
-        ArrayList<String> verteiler = DatabaseHandler.getDBReader().loadNewQuestion();
-        proposal1.setText(verteiler.get(0));
-        proposal2.setText(verteiler.get(1));
-        proposal3.setText(verteiler.get(2));
-        proposal4.setText(verteiler.get(3));
-        word.setText(verteiler.get(4));
+        delay();
+        loadNewQuestion();
     }
 
     public void handleclick3() {
@@ -98,14 +91,8 @@ public class PrimaryController {
         } else {
             highlightWrongAnswer(3);
         }
-
-        //load new question
-        ArrayList<String> verteiler = DatabaseHandler.getDBReader().loadNewQuestion();
-        proposal1.setText(verteiler.get(0));
-        proposal2.setText(verteiler.get(1));
-        proposal3.setText(verteiler.get(2));
-        proposal4.setText(verteiler.get(3));
-        word.setText(verteiler.get(4));
+        delay();
+        loadNewQuestion();
     }
 
     public void handleclick4() {
@@ -118,14 +105,8 @@ public class PrimaryController {
             highlightWrongAnswer(4);
         }
 
-
-        //load new question
-        ArrayList<String> verteiler = DatabaseHandler.getDBReader().loadNewQuestion();
-        proposal1.setText(verteiler.get(0));
-        proposal2.setText(verteiler.get(1));
-        proposal3.setText(verteiler.get(2));
-        proposal4.setText(verteiler.get(3));
-        word.setText(verteiler.get(4));
+        delay();
+        loadNewQuestion();
     }
 
     public void handleclick5()  {
@@ -153,27 +134,31 @@ public class PrimaryController {
 
     public void highlightCorrectAnswer(int buttonId)  {
 
-        if (buttonId == 1) {
+           if (buttonId == 1) {
+            String style1 = proposal1.getStyle();
             proposal1.setStyle("-fx-background-color: #00CC00");
+            PauseTransition wait1 = new PauseTransition(Duration.seconds(2));
+            wait1.setOnFinished(event -> proposal1.setStyle(style1));
+            wait1.play();
         } else if (buttonId == 2) {
+            String style2 = proposal2.getStyle();
             proposal2.setStyle("-fx-background-color: #00CC00");
+            PauseTransition wait2 = new PauseTransition(Duration.seconds(2));
+            wait2.setOnFinished(event -> proposal1.setStyle(style2));
+            wait2.play();
         } else if (buttonId == 3) {
+            String style3 = proposal3.getStyle();
             proposal3.setStyle("-fx-background-color: #00CC00");
+            PauseTransition wait3 = new PauseTransition(Duration.seconds(2));
+            wait3.setOnFinished(event -> proposal3.setStyle(style3));
+            wait3.play();
         } else if (buttonId == 4) {
+            String style4 = proposal4.getStyle();
             proposal4.setStyle("-fx-background-color: #00CC00");
+            PauseTransition wait4 = new PauseTransition(Duration.seconds(2));
+            wait4.setOnFinished(event -> proposal4.setStyle(style4));
+            wait4.play();
         }
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-       /**
-        proposal1.setStyle("-fx-background-color: lightgrey");
-        proposal2.setStyle("-fx-background-color: lightgrey");
-        proposal3.setStyle("-fx-background-color: lightgrey");
-        proposal4.setStyle("-fx-background-color: lightgrey");
-        }
- */
     }
 
     public void highlightWrongAnswer(int buttonId){
@@ -198,6 +183,22 @@ public class PrimaryController {
             e.printStackTrace();
         }
 
+    }
+
+    public void loadNewQuestion(){
+        delay();
+        ArrayList<String> verteiler = DatabaseHandler.getDBReader().loadNewQuestion();
+        proposal1.setText(verteiler.get(0));
+        proposal2.setText(verteiler.get(1));
+        proposal3.setText(verteiler.get(2));
+        proposal4.setText(verteiler.get(3));
+        word.setText(verteiler.get(4));
+    }
+
+    public void delay(){
+        PauseTransition wait = new PauseTransition(Duration.seconds(4));
+        //wait.setOnFinished(event -> );
+        wait.play();
     }
 }
 
