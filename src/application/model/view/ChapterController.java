0bc157d7a1062b308be7034339f2305a1665033e
phkorner <Philipp@140183>
2019-12-14 10:01:11 +0100

    Comments

--- src/application/model/view/ChapterController.java
+++ src/application/model/view/ChapterController.java
@@ -18,26 +18,31 @@ public class ChapterController {
     @FXML
     Button family;
 
+    // Loads new questions to the topic weather
     public void weatherClick(){
         DatabaseHandler.getDBReader().newChapter("Weather.txt");
         MainApplication.closeChapterStage();
     }
 
+    // Loads new questions to the topic sports
     public void sportsClick(){
         DatabaseHandler.getDBReader().newChapter("Sports.txt");
         MainApplication.closeChapterStage();
     }
 
+    // Loads new questions to the topic professions
     public void professionsClick(){
         DatabaseHandler.getDBReader().newChapter("Professions.txt");
         MainApplication.closeChapterStage();
     }
 
+    // Loads new questions to the topic food
     public void foodClick(){
         DatabaseHandler.getDBReader().newChapter("Food.txt");
         MainApplication.closeChapterStage();
     }
 
+    // Loads new questions to the topic family
     public void familyClick(){
         DatabaseHandler.getDBReader().newChapter("Family.txt");
         MainApplication.closeChapterStage();
